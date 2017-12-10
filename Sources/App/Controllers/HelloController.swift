1006fb9b8d94c12d0b806f8b8bedc1471bfb095a
scauos <645670644@qq.com>
2017-12-10 14:51:09 +0000

    third commit

--- Sources/App/Controllers/HelloController.swift
+++ Sources/App/Controllers/HelloController.swift
@@ -12,7 +12,7 @@ final class HelloController: ResourceRepresentable {
     /// GET /hello
     func index(_ req: Request) throws -> ResponseRepresentable {
         return try view.make("hello", [
-            "name": "大朕东"
+            "name": "大朕东是真的牛逼"
         ], for: req)
     }
 
