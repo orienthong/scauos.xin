8bd1b2719886a330431aadd1fa872ee7abdd47c4
scauos <645670644@qq.com>
2017-12-16 00:33:33 +0800

    🐖颜小姐

--- Sources/App/Controllers/HelloController.swift
+++ Sources/App/Controllers/HelloController.swift
@@ -12,7 +12,7 @@ final class HelloController: ResourceRepresentable {
     /// GET /hello
     func index(_ req: Request) throws -> ResponseRepresentable {
         return try view.make("hello", [
-            "name": "大朕东是真的牛逼"
+            "name": "🐷颜小姐考个好成绩！"
         ], for: req)
     }
 
